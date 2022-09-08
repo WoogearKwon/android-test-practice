4900b838063efcbae48084a3858fb1640651bb50
woogear.kwon <techkwon1717@hanmail.net>
2022-09-09 06:07:44 +0900

    add test for saving and reading activity

--- app/src/androidTest/java/eu/maxkim/boredombuster/activity/framework/datasource/ActivityLocalDataSourceImplTest.kt
+++ app/src/androidTest/java/eu/maxkim/boredombuster/activity/framework/datasource/ActivityLocalDataSourceImplTest.kt
@@ -3,11 +3,15 @@ package eu.maxkim.boredombuster.activity.framework.datasource
 import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
+import eu.maxkim.boredombuster.activity.fake.usecase.activity1
 import eu.maxkim.boredombuster.activity.framework.db.ActivityDao
 import eu.maxkim.boredombuster.framework.AppDatabase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Test
 
 class ActivityLocalDataSourceImplTest {
     private lateinit var activityDao: ActivityDao
@@ -23,5 +27,18 @@ class ActivityLocalDataSourceImplTest {
     @After
     fun closeDb() {
         database.close()
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun canSaveActivityToTheDbAndReadIt() = runTest{
+        // Arrange
+        val activityLocalDataSource = ActivityLocalDataSourceImpl(activityDao)
+
+        // Act
+        activityLocalDataSource.saveActivity(activity1)
+
+        // Assert
+        assert(activityLocalDataSource.isActivitySaved(activity1.key))
     }
 }
