36007d76899e39b89d5604a40cdcb20d5a90680d
woogear.kwon <techkwon1717@hanmail.net>
2022-09-12 21:43:30 +0900

    응답 샘플 객체 & json 추가

---
+++ app/src/test/java/eu/maxkim/boredombuster/activity/framework/datasource/TestModels.kt
@@ -0,0 +1,13 @@
+package eu.maxkim.boredombuster.activity.framework.datasource
+
+import eu.maxkim.boredombuster.activity.model.Activity
+
+val responseActivity = Activity(
+    name = "Go to a music festival with some friends",
+    type = Activity.Type.Social,
+    participantCount = 4,
+    price = 0.4f,
+    accessibility = 0.2f,
+    key = "6482790",
+    link = ""
+)
